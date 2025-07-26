0ed6c7aa00752742e01e7c52d03eb9a7ab404cac
dineshkrishna9999 <kdineshkvkl@gmail.com>
2025-07-26 22:33:20 +0530

    feat - add docker file

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
-async def home():
-    return "Welcome to the home page!"
+async def home()->dict:
+    return {
+        "message": "Welcome to the home page!",
+        "config": {
+            "A_CONFIG": os.getenv("A_CONFIG","I am default"),
+            "A_SECRET": os.getenv("A_SECRET","I am secret")
+        }
+    }
 
 @app.get("/health")
-async def health():
+async def health()->dict:
     return {"status": "All is well! Be Happy!"}
--- backlog/tasks/task-4 - create-a-docker-file-and-access-the-.env-params-in-fastapi-app.md
+++ backlog/tasks/task-4 - create-a-docker-file-and-access-the-.env-params-in-fastapi-app.md
@@ -1,7 +1,7 @@
 ---
-id: task-2
-title: Configure the aws account
-status: To Do
+id: task-4
+title: create a docker file and access the .env params in fastapi app
+status: Done
 assignee: []
 created_date: '2025-07-26'
 updated_date: '2025-07-26'
